63707259d9b4164b4912f90b6e1f8e44210da71c
David Pinilla <metroid_300@hotmail.com>
2019-01-14 11:58:58 +0100

    Post structure created and G Materials added

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+import {Post} from '../post.model';
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+
+  // posts=[
+  //   {title: 'First Post', content:'First post content'},
+  //   {title: 'Second Post', content:'Second post content'},
+  //   {title: 'Third Post', content:'Third post content'},
+  // ]
+  @Input()
+  posts:Post[]=[]
+}
