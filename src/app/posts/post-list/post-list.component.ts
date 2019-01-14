36abb9e9c57617fcf6dcb9578399e48b59e25b7a
David Pinilla <metroid_300@hotmail.com>
2019-01-14 16:52:18 +0100

    Get Post methods (in case of updating postlist)added using rxjs6

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,13 +1,25 @@
-import { Component, Input } from '@angular/core';
-
-import {Post} from '../post.model';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostsService } from '../posts.service'
+import { Post } from '../post.model';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
-
-  @Input()
-  posts:Post[]=[]
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub: Subscription;
+  constructor(public postsService: PostsService) {
+  }
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener().
+      subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
